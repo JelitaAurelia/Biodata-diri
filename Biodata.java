5ce934d0f09df314bb842aa53d759423731f8a11
JelitaAurelia <12350123140@students.uin-suska.ac.id>
2024-09-10 17:38:18 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,16 @@
+package Tugas;
+
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Jelita Aurelia";
+        String nim = "12350123140";
+        String hobi = "Menonton dan membaca";
+        System.out.println("=================================");
+        System.out.println("        BIODATA DIRI");
+        System.out.println("=================================");
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("Hobi   : " + hobi);
+        System.out.println("=================================");
+    }
+}
